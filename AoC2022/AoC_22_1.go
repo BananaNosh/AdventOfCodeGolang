1ba4bdf88b6a9523c52e54e20e3f87b3f5ad5cbe
Jonathan Loos <loos.jonathan.martin@gmail.com>
2022-12-02 13:24:23 +0100

    Add 2022 1

---
+++ AoC2022/AoC_22_1.go
@@ -0,0 +1,38 @@
+package AoC22
+
+import (
+	"AoC/utils/collections"
+	"AoC/utils/date"
+	"AoC/utils/io"
+	"AoC/utils/math"
+	"AoC/utils/requests"
+	"AoC/utils/types"
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func AoC1() {
+	year := 2022
+	day := 1
+	fmt.Println("On " + date.DateStringForDay(year, day) + ":")
+	elves := io.ReadAndSplitInput("\n\n", 2022)
+	fmt.Println("Part 1:")
+	weightsPerElve := collections.Map(elves, func(elf string) []int {
+		return types.ToIntSlice(strings.Split(elf, "\n"))
+	})
+	fmt.Println(weightsPerElve)
+	totalPerElve := collections.Map(weightsPerElve, func(weights []int) int {
+		return collections.Sum(weights)
+	})
+	max := math.MaxInSlice(totalPerElve)
+	fmt.Println(max)
+	//requests.SendAnswer(day, year, max, 1)
+	fmt.Println("Part 2:")
+	sort.Ints(totalPerElve)
+	fmt.Println(totalPerElve[:3])
+	//maxMultiple := totalPerElve[len(totalPerElve)-3:]
+	maxMultiple := collections.Sum(totalPerElve[len(totalPerElve)-3:])
+	fmt.Println(maxMultiple)
+	requests.SendAnswer(day, year, maxMultiple, 2)
+}
