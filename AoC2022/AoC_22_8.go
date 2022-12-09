cd39224fd7de17b15d3b462c90379fced24e17ff
Jonathan Loos <loos.jonathan.martin@gmail.com>
2022-12-09 02:13:55 +0100

    Add 2022 8

--- AoC2022/AoC_22_8.go
+++ AoC2022/AoC_22_8.go
@@ -16,6 +16,9 @@ func scanMap(grid [][]int, reverse bool) ([][]int, [][]int) {
 		grid = reversed(grid)
 	}
 	columnMax := make([]int, len(grid[0]))
+	for i := range columnMax {
+		columnMax[i] = -1
+	}
 	for i, row := range grid {
 		rowMax := -1
 		for j, tree := range row {
@@ -25,7 +28,7 @@ func scanMap(grid [][]int, reverse bool) ([][]int, [][]int) {
 			} else {
 				scanLeft[i] = append(scanLeft[i], 0)
 			}
-			if tree > columnMax[j] || columnMax[j] == 0 {
+			if tree > columnMax[j] {
 				columnMax[j] = tree
 				scanTop[i] = append(scanTop[i], 1)
 			} else {
@@ -57,13 +60,95 @@ func printGrid[T any](grid [][]T) {
 }
 
 func ewiseOr(grid1 [][]int, grid2 [][]int) [][]int {
+	return ewiseFunc(grid1, grid2, func(l int, r int) int {
+		return math.Min(1, l+r)
+	})
+}
+
+func ewiseMul(grid1 [][]int, grid2 [][]int) [][]int {
+	return ewiseFunc(grid1, grid2, func(l int, r int) int {
+		return l * r
+	})
+}
+
+func ewiseFunc(grid1 [][]int, grid2 [][]int, f func(int, int) int) [][]int {
 	result := make([][]int, len(grid1))
 	for i, row1 := range grid1 {
 		for j, value1 := range row1 {
-			result[i] = append(result[i], math.Min(1, value1+grid2[i][j]))
+			result[i] = append(result[i], f(value1, grid2[i][j]))
 		}
 	}
 	return result
+}
+
+func countSeenTrees(grid [][]int) int {
+	leftScan, topScan := scanMap(grid, false)
+	rightScan, bottomScan := scanMap(grid, true)
+	//printGrid(leftScan)
+	//printGrid(topScan)
+	//printGrid(rightScan)
+	//printGrid(bottomScan)
+	seenTrees := ewiseOr(leftScan, topScan)
+	seenTrees = ewiseOr(rightScan, seenTrees)
+	seenTrees = ewiseOr(bottomScan, seenTrees)
+	//fmt.Println("SeenTrees")
+	//printGrid(seenTrees)
+	seenCount := collections.Sum(collections.Map(seenTrees, func(row []int) int {
+		return collections.Sum(row)
+	}))
+	return seenCount
+}
+
+func scanForTreeHouse(grid [][]int, reverse bool) ([][]int, [][]int) {
+	scanLeft := make([][]int, len(grid))
+	scanTop := make([][]int, len(grid))
+	if reverse {
+		grid = reversed(grid)
+	}
+	columnMaxs := make([]map[int]int, len(grid[0]))
+	for i := range columnMaxs {
+		columnMaxs[i] = make(map[int]int)
+	}
+	for i, row := range grid {
+		rowMax := make(map[int]int)
+		for j, tree := range row {
+			if collections.HasKey(rowMax, tree) {
+				scanLeft[i] = append(scanLeft[i], j-rowMax[tree])
+			} else {
+				scanLeft[i] = append(scanLeft[i], j)
+			}
+			if collections.HasKey(columnMaxs[j], tree) {
+				scanTop[i] = append(scanTop[i], i-columnMaxs[j][tree])
+			} else {
+				scanTop[i] = append(scanTop[i], i)
+			}
+			for k := 0; k <= tree; k++ {
+				rowMax[k] = j
+				columnMaxs[j][k] = i
+			}
+		}
+	}
+	if reverse {
+		return reversed(scanLeft), reversed(scanTop)
+	}
+	return scanLeft, scanTop
+}
+
+func findTreeHouse(grid [][]int) int {
+	left, top := scanForTreeHouse(grid, false)
+	right, bottom := scanForTreeHouse(grid, true)
+	printGrid(left)
+	printGrid(top)
+	printGrid(right)
+	printGrid(bottom)
+	treeScore := ewiseMul(left, top)
+	treeScore = ewiseMul(treeScore, right)
+	treeScore = ewiseMul(treeScore, bottom)
+	printGrid(treeScore)
+	bestScore := collections.MaxNumber(collections.Map(treeScore, func(row []int) int {
+		return collections.MaxNumber(row)
+	}))
+	return bestScore
 }
 
 func AoC8() {
@@ -77,23 +162,11 @@ func AoC8() {
 	input := io.ReadInputAs2DInts(8, 2022)
 	printGrid(input)
 	fmt.Println("Part 1:")
-	leftScan, topScan := scanMap(input, false)
-	rightScan, bottomScan := scanMap(input, true)
-	printGrid(leftScan)
-	printGrid(topScan)
-	printGrid(rightScan)
-	printGrid(bottomScan)
-	seenTrees := ewiseOr(leftScan, topScan)
-	seenTrees = ewiseOr(rightScan, seenTrees)
-	seenTrees = ewiseOr(bottomScan, seenTrees)
-	fmt.Println("SeenTrees")
-	printGrid(seenTrees)
-	seenCount := collections.Sum(collections.Map(seenTrees, func(row []int) int {
-		return collections.Sum(row)
-	}))
+	seenCount := countSeenTrees(input)
 	fmt.Println("seen", seenCount)
 	requests.SubmitAnswer(day, year, seenCount, 1)
 
 	fmt.Println("Part 2:")
-	// requests.SubmitAnswer(day, year, 0, 2)
+	score := findTreeHouse(input)
+	requests.SubmitAnswer(day, year, score, 2)
 }
